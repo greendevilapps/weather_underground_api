55106f35971f16d77159104380ddf86e26fa8bd4
thestelz <stelzera24@gmail.com>
2015-08-27 09:58:23 -0400

    Updated doc and readme

--- README.md
+++ README.md
@@ -26,17 +26,20 @@ Create a new class by passing in your api key
 
 Supported options:
 
-- raise_errors: Will `raise` an error when `true` or just return the error message when `false`, when calling the helper method `response_has_error?`.
+- raise_errors: Will `raise` an error when `true` or just return the error message when `false`, when calling the helper method `response_has_error?`. (defaults: false).
 - raise_api_error: If no key passed in during initialization and this is set to `true` it will `raise` an error.
 - format: The format that you wish to do all of the calls by. Only supported option right now is `json`.
 - lang: Language to be used. (defaults: 'EN').
 - pws: Use personal weather stations for conditions. (defaults: '1')
 - bestfct: Use Weather Underground Best Forecast for forecast. (defaults: '1')
+- icon_set: The icon set to use. See [weather underground icon sets](http://www.wunderground.com/weather/api/d/docs?d=resources/icon-sets&apiref=77eba12431a68f79) for more details.
     
 Supported features
 ------------------
 
 **AutoComplete API**
+
+Returns a list of locations or hurricanes which match against a partial query.
 
     results = @wu.autocomplete(query)
     
@@ -47,6 +50,8 @@ For all data attrs see: [autocomplete-api](http://www.wunderground.com/weather/a
 
 **Geolookup**
 
+Returns the city name, zip code / postal code, latitude-longitude coordinates and nearby personal weather stations.
+
     @geo = @wu.geolookup(query)
     
     $ $geo['location']['state']
@@ -55,6 +60,8 @@ For all data attrs see: [autocomplete-api](http://www.wunderground.com/weather/a
 For all data attrs see: [data/geolookup](http://www.wunderground.com/weather/api/d/docs?d=data/geolookup&apiref=77eba12431a68f79)
 
 **Conditions**
+
+Returns the current temperature, weather condition, humidity, wind, 'feels like' temperature, barometric pressure, and visibility.
 
     @current_conditions = @wu.conditions(query)
     
@@ -65,6 +72,8 @@ For all data attrs see: [data/conditions](http://www.wunderground.com/weather/ap
 
 **Forecast**
 
+Returns a summary of the weather for the next 3 days. This includes high and low temperatures, a string text forecast and the conditions.
+
     @forecast = @wu.forecast(query)
     
     $ @forecast['forecast']['simpleforecast']['forecastday'][0]['pop']
@@ -73,6 +82,9 @@ For all data attrs see: [data/conditions](http://www.wunderground.com/weather/ap
 For all data attrs see: [data/forecast](http://www.wunderground.com/weather/api/d/docs?d=data/forecast&apiref=77eba12431a68f79)
 
 **Alerts**
+
+Returns the short name description, expiration time and a long text description of a severe alert, 
+if one has been issued for the searched upon location.
 
     @alerts = @wu.alerts(query)
     
@@ -85,6 +97,8 @@ For all data attrs see: [data/alerts](http://www.wunderground.com/weather/api/d/
 
 **Hourly**
 
+Returns the next 36 hours from current time.
+
     @hourly = @wu.hourly(query)
     
     $ @hourly['hourly_forecast'][0]['temp']['english']
@@ -93,6 +107,8 @@ For all data attrs see: [data/alerts](http://www.wunderground.com/weather/api/d/
 For all data attrs see: [data/hourly](http://www.wunderground.com/weather/api/d/docs?d=data/hourly&apiref=77eba12431a68f79)
 
 **Forecast10day**
+
+Returns a summary of the weather for the next 10 days. This includes high and low temperatures, a string text forecast and the conditions.
 
 You can call `extended_forecast` or `forecast10day`.
 
@@ -105,25 +121,60 @@ For all data attrs see: [data/forecast10day](http://www.wunderground.com/weather
 
 **Radar**
 
+Returns a static or animated radar image for a given location.
+
 Radar function will only return a string url. This is so you can place it in an image_tag or serve it up to any of your assets.
 
     $ @wu.radar_url(query)
     => "http://api.wunderground.com/api/API_KEY/radar/q/QUERY.png?width=300&height=300&radius=100&newmaps=1"
     $ @wu.radar_url(query, width: '400', height: '400')
     => "http://api.wunderground.com/api/API_KEY/radar/q/QUERY.png?width=400&height=400&radius=100&newmaps=1"
+    $ @wu.radar_url(query, query_type: 'animatedradar', type: 'gif')
+    => "http://api.wunderground.com/api/API_KEY/animatedradar/q/QUERY.gif?width=300&height=300&radius=100&newmaps=1"
 
 Required options that you can pass into the `radar_url` function are:
 
-- type: gif, png, or swf (defaults: png)
-- radius: 100 is close, 1000 is country (defaults: 100)
-- width: Width of image in pixels (defaults: 300)
-- height: Height of image in pixels (defaults: 300)
-- newmaps: Transparent image (default) or show basemap (defaults: 1)
+- :query_type Expects a string. Defaults to 'radar'. Choices are: 'radar' and 'animatedradar'.
+
+_Note:_ If using 'animatedradar', you will need to pass in 'gif' or 'swf' as the :type or the request will fail.
 
 For all data attrs see: [data/radar](http://www.wunderground.com/weather/api/d/docs?d=layers/radar&apiref=77eba12431a68f79)
 
+**Satellite**
+
+Returns a static or animated satellite image for a given location.
+
+Satellite function will only return a string url. This is so you can place it in an image_tag or serve it up to any of your assets.
+
+    $ @wu.satellite_url(query)
+    => "http://api.wunderground.com/api/API_KEY/satellite/q/QUERY.png?width=300&height=300&radius=100&basemap=1"
+    $ @wu.satellite_url(query, width: '400', height: '400')
+    => "http://api.wunderground.com/api/API_KEY/satellite/q/QUERY.png?width=400&height=400&radius=100&basemap=1"
+    $ @wu.satellite_url(query, query_type: 'animatedsatellite', type: 'gif')
+    => "http://api.wunderground.com/api/API_KEY/animatedsatellite/q/QUERY.gif?width=300&height=300&radius=100&basemap=1"
+
+- :query_type Expects a string. Defaults to 'satellite'. Choices are: 'satellite' and 'animatedsatellite'.
+
+_Note:_ If using 'animatedsatellite', you will need to pass in 'gif' as the :type or the request will fail.
+
+For all data attrs see: [data/satellite](http://www.wunderground.com/weather/api/d/docs?d=layers/satellite&apiref=77eba12431a68f79)
+
 Helper functions
 ----------------
+
+**Checking api key**
+
+Check the api key
+
+    $ @wu.api_key
+    => API_KEY
+
+**Checking options**
+
+Check the options that are currently being used
+
+    $ @wu.options
+    => {:raise_errors=>false,:raise_api_error=>true,:format=>"json",:lang=>"EN",:pws=>"1",:bestfct=>"1",:icon_set=>"k"}
 
 **Checking for errors**
 
@@ -136,12 +187,17 @@ This will return `nil` if there is no error. If there is an error it will either
     => nil
 
 
-**Radar url data-attrs**
+**Icon set url**
 
-This will create `data-attrs` for radar conditions.
+This will return a url string for the source of weather underground's api icon sets.
 
-    @wu.radar_data_attrs(query)
-    => 'data-radar-height="300" data-radar-newmaps="1" data-radar-query-url="http://api.wunderground.com/api/API_KEY/radar/q/" data-radar-query="QUERY" data-radar-radius="100" data-radar-type="png" data-radar-width="300"'
+    @wu.icon_url('partlycloudy')
+    => "http://icons.wxug.com/i/c/k/partlycloudy.gif"
+
+To check out the icon-sets, click [here](http://www.wunderground.com/weather/api/d/docs?d=resources/icon-sets&apiref=77eba12431a68f79).
+
+_Note:_ The icon set that is provided by Weather Underground. These are sent as .gif's and typically don't scale well. 
+You might want to use a different icon set or use and icon font.
 
 Contributing
 ------------
--- lib/weather_underground_api.rb
+++ lib/weather_underground_api.rb
@@ -5,13 +5,40 @@ require 'json'
 require 'net/http'
 
 module WeatherUndergroundApi
+    # Main API url
     API_URL = 'http://api.wunderground.com/api/%s'
+
+    # Autocomplete url, for searching places
     AUTOCOMPLETE_URL = 'http://autocomplete.wunderground.com/aq' #?query=%s
 
+    # Add error classes
     class WuError < StandardError; end
     class MissingKey < WuError; end
 
+    # The main class that will initialize everything
     class Base
+        # This is called when `new` is called on this class.
+        # +api_key+ is required and is expecting a string.
+        # +options+ are a set of attrs that can change the defaults.
+        #
+        # Supported options:
+        #   Name               | Type           | Default   | Info
+        #   -------------------------------------------------------
+        #   +:raise_errors+    | Boolean        | false     | This option is `false` by default. It is used to control whether
+        #                                                     or not to throw errors when calling the helper function `response_has_error?`.
+        #   +:raise_api_error+ | Boolean        | true      | This option is `true` by default. This will raise an error
+        #                                                     when no api_key has been passed into the initialize method.
+        #   +:format+          | String         | 'json'    | The type of format that you want to use. The default is `'json'`.
+        #                                                     As of right now that is the only format supported. XML may come in the future.
+        #   +:lang+            | String         | 'EN'      | Is the language that you want to use. By default it uses english ('EN').
+        #                                                     Just pass in the language code found [here](http://www.wunderground.com/weather/api/d/docs?d=language-support).
+        #   +:pws+             | String/Boolean | '1'       | If you want to use personal weather stations for conditions pass in '1',
+        #                                                     which is the default, or pass in '0' to turn this off.
+        #   +:bestfct+         | String/Boolean | '1'       | This will use Weather Underground's "best forecast" feature.
+        #                                                     Learn more [here](http://www.wunderground.com/about/data.asp).
+        #   +:icon_set+        | String         | 'k'       | The icon set that is provided by Weather Underground. These are sent as .gif's
+        #                                                     and typically don't scale well. You might want to use a different icon set or
+        #                                                     use and icon font.
         def initialize(api_key, options = {})
             @api_key = api_key
 
@@ -23,23 +50,166 @@ module WeatherUndergroundApi
                 :format          => 'json', # 'json'
                 :lang            => 'en',   # 'lang:en'
                 :pws             => '1',    # 'pws:1'
-                :bestfct         => '1'     # 'bestfct:1'
+                :bestfct         => '1',    # 'bestfct:1',
+                :icon_set        => 'k'     # Default to last on the list, which is 'k'
             )
             @options[:lang].upcase! unless @options[:lang].blank?
+            @options[:pws] = is_true(@options[:pws]) ? '1' : '0'
+            @options[:bestfct] = is_true(@options[:bestfct]) ? '1' : '0'
 
             if @api_key.blank? && @options[:raise_api_error]
                 raise(MissingKey, "API Key not found!")
             end
         end
 
+        # Returns a list of locations or hurricanes which match against a partial query.
+        #
+        # +query+ Is the location query that needs to be ran against. This is required.
+        # +options+ Are a set of options for this request.
+        #
+        # Supported options:
+        #   - +:format+ Can override the default format. This may mess up any calls when not
+        #               using json. Defaults to `@options[:format]`.
+        #   - +:h+ Whether or not to include hurricanes in results. Defaults to '0'.
+        #
+        # To see more options please see [this](http://www.wunderground.com/weather/api/d/docs?d=autocomplete-api&apiref=77eba12431a68f79).
+        def autocomplete(query, options = {})
+            options ||= {}
+            options.reverse_merge! :format => @options[:format], :h => '0'
+            options[:h] = is_true(options[:h]) ? '1' : '0'
+            get_json("#{autocomplete_url}?format=#{options.delete(:format)}&h=#{options.delete(:h)}&query=#{parse_query(query)}")
+        end
+
+        # Returns the city name, zip code / postal code, latitude-longitude coordinates and nearby personal weather stations.
+        #
+        # +query+ Is the query location name.
+        #
+        # For more info see [this](http://www.wunderground.com/weather/api/d/docs?d=data/geolookup&apiref=77eba12431a68f79).
+        def geolookup(query)
+            get_json("#{api_url}/geolookup/#{url_settings}/q/#{parse_query(query)}.#{@options[:format]}")
+        end
+
+        # Returns the current temperature, weather condition, humidity, wind,
+        # 'feels like' temperature, barometric pressure, and visibility.
+        #
+        # +query+ The query to run a location against.
+        #
+        # See more [here](http://www.wunderground.com/weather/api/d/docs?d=data/conditions&apiref=77eba12431a68f79).
+        def conditions(query)
+            get_json("#{api_url}/conditions/#{url_settings}/q/#{parse_query(query)}.#{@options[:format]}")
+        end
+
+        # Returns the short name description, expiration time and a long text description of a severe alert,
+        # if one has been issued for the searched upon location.
+        #
+        # +query+ The location query.
+        #
+        # See more about alerts [here](http://www.wunderground.com/weather/api/d/docs?d=data/alerts&apiref=77eba12431a68f79).
+        def alerts(query)
+            get_json("#{api_url}/alerts/#{url_settings}/q/#{parse_query(query)}.#{@options[:format]}")
+        end
+
+        # Returns a summary of the weather for the next 3 days. This includes high and low temperatures,
+        # a string text forecast and the conditions.
+        #
+        # +query+ Location query to find.
+        #
+        # Find out more [here](http://www.wunderground.com/weather/api/d/docs?d=data/forecast&apiref=77eba12431a68f79).
+        def forecast(query)
+            get_json("#{api_url}/forecast/q/#{parse_query(query)}.#{@options[:format]}")
+        end
+
+        # Returns a summary of the weather for the next 10 days. This includes high and low temperatures,
+        # a string text forecast and the conditions.
+        #
+        # +query+ Location query.
+        #
+        # See [this](http://www.wunderground.com/weather/api/d/docs?d=data/forecast10day&apiref=77eba12431a68f79) for more.
+        def extended_forecast(query)
+            get_json("#{api_url}/forecast10day/q/#{parse_query(query)}.#{@options[:format]}")
+        end
+        # Weather underground uses the call `forecast10day`, so with trying to keep with the same names,
+        # this is here to support that.
+        alias_method :forecast10day, :extended_forecast
+
+        # Returns the next 36 hours from current time.
+        #
+        # +query+ Location query
+        #
+        # For more see [this](http://www.wunderground.com/weather/api/d/docs?d=data/hourly&apiref=77eba12431a68f79).
+        def hourly(query)
+            get_json("#{api_url}/hourly/q/#{parse_query(query)}.#{@options[:format]}")
+        end
+
+        # Returns a static or animated radar image for a given location.
+        #
+        # +query+ The location query
+        # +options+ Options to pass in the url.
+        #
+        # To change the radar from just being an image to an animation, you need to change
+        # the option +query_type+ to 'animatedradar'. When changing this to that type,
+        # you must pass in the type as 'gif' or 'swf' format or the request will fail.
+        #
+        # To see all of the options see [here](http://www.wunderground.com/weather/api/d/docs?d=layers/radar&apiref=77eba12431a68f79).
+        def radar_url(query, options = {})
+            options ||= {}
+            options.stringify_keys!.reverse_merge!(
+                'query_type' => 'radar',
+                'type'       => 'png',
+                'radius'     => '100',
+                'width'      => '300',
+                'height'     => '300',
+                'newmaps'    => '1'
+            )
+            options['query_type'] = 'radar' if options['query_type'].blank?
+            "#{api_url}/#{options.delete('query_type')}/q/#{parse_query(query)}.#{options.delete('type')}?#{to_params(options)}"
+        end
+
+        # Returns a static or animated satellite image for a given location.
+        #
+        # +query+ Location query
+        # +options+ Options to be passed in the request.
+        #
+        # To change the radar from just being an image to an animation, you need to change
+        # the option +query_type+ to 'animatedsatellite'. When changing this to that type,
+        # you must pass in the type as 'gif' format or the request will fail.
+        #
+        # See [here](http://www.wunderground.com/weather/api/d/docs?d=layers/satellite&apiref=77eba12431a68f79) for a list of options.
+        def satellite_url(query, options = {})
+            options ||= {}
+            options.stringify_keys!.reverse_merge!(
+                'query_type' => 'satellite',
+                'type'       => 'png',
+                'radius'     => '100',
+                'width'      => '300',
+                'height'     => '300',
+                'basemap'    => '1'
+            )
+            "#{api_url}/#{options.delete('query_type')}/q/#{parse_query(query)}.#{options.delete('type')}?#{to_params(options)}"
+        end
+
+        # --------------
+        # Helper methods
+        # --------------
+
+        # Returns the api_key
         def api_key
             @api_key
         end
 
+        # Returns the hash of options.
         def options
             @options
         end
 
+        # Will check a response object if it has an error or not. You must pass in the whole response
+        # object or this may not pick up on the error.
+        #
+        # +response+ The full response from Weather Underground.
+        #
+        # If +@options[:raise_errors]+ is set to true, this will raise an error. If that is set to false,
+        # which is the default, it will return the error as a string. By default it will return `nil` if
+        # there was no error found.
         def response_has_error?(response)
             if response && response['response'] && response['response']['error']
                 error = "#{response['response']['error']['type']}: #{response['response']['error']['description']})"
@@ -47,57 +217,13 @@ module WeatherUndergroundApi
             end
         end
 
-        # http://www.wunderground.com/weather/api/d/docs?d=autocomplete-api
-        def autocomplete(query, options = {})
-            options ||= {}
-            options.reverse_merge! :format => @options[:format].to_s.upcase, :h => '0'
-            get_json("#{autocomplete_url}?format=#{options.delete(:format)}&h=#{options.delete(:h)}&query=#{parse_query(query)}")
+        # Returns a url string for the given icon. To set the icon-set that is used, please see
+        # +initialize+ for options to be passed in.
+        def icon_url(icon)
+            icon_set_url % icon
         end
 
-        # http://www.wunderground.com/weather/api/d/docs?d=data/geolookup
-        def geolookup(query)
-            get_json("#{api_url}/geolookup/#{url_settings}/q/#{parse_query(query)}.#{@options[:format]}")
-        end
-
-        # http://www.wunderground.com/weather/api/d/docs?d=data/conditions
-        def conditions(query)
-            get_json("#{api_url}/conditions/#{url_settings}/q/#{parse_query(query)}.#{@options[:format]}")
-        end
-
-        # http://www.wunderground.com/weather/api/d/docs?d=data/alerts
-        def alerts(query)
-            #JSON.parse( Net::HTTP.get_response( URI("#{build_url}/alerts/#{settings}/q/#{parse_query(location_query)[1]}.json") ).body )
-            get_json("#{api_url}/alerts/#{url_settings}/q/#{parse_query(query)}.#{@options[:format]}")
-        end
-
-        # http://www.wunderground.com/weather/api/d/docs?d=data/forecast
-        def forecast(query)
-            get_json("#{api_url}/forecast/q/#{parse_query(query)}.#{@options[:format]}")
-        end
-
-        # http://www.wunderground.com/weather/api/d/docs?d=data/forecast10day
-        def extended_forecast(query)
-            get_json("#{api_url}/forecast10day/q/#{parse_query(query)}.#{@options[:format]}")
-        end
-        alias_method :forecast10day, :extended_forecast
-
-        # http://www.wunderground.com/weather/api/d/docs?d=data/hourly
-        def hourly(query)
-            get_json("#{api_url}/hourly/q/#{parse_query(query)}.#{@options[:format]}")
-        end
-
-        def radar_url(query, options = {})
-            options ||= {}
-            options.stringify_keys!.reverse_merge!(
-                'type'    => 'png',
-                'radius'  => '100',
-                'width'   => '300',
-                'height'  => '300',
-                'newmaps' => '1'
-            )
-            "#{api_url}/radar/q/#{parse_query(query)}.#{options.delete('type')}?#{to_params(options)}"
-        end
-
+        # :nodoc:
         def radar_data_attrs(query, options = {}, escape = true)
             options ||= {}
             options.stringify_keys!.reverse_merge!(
@@ -119,6 +245,10 @@ module WeatherUndergroundApi
 
         private
 
+        def is_true(str)
+            /(1|true|yes)/i =~ str.to_s
+        end
+
         def parse_query(query)
             query.to_s.gsub(/\s+/, '')
         end
@@ -132,7 +262,11 @@ module WeatherUndergroundApi
         end
 
         def url_settings
-            "lang:#{@options[:lang]}/pws:#{@options[:pws]}/bestfct:#{@options[:bestfct]}"
+            settings_for_url = []
+            settings_for_url << "lang:#{@options[:lang]}" unless @options[:lang].blank?
+            settings_for_url << "pws:#{@options[:pws]}" unless @options[:pws].blank?
+            settings_for_url << "bestfct:#{@options[:bestfct]}" unless @options[:bestfct].blank?
+            settings_for_url.join('/')
         end
 
         def get_json(url)
@@ -140,7 +274,7 @@ module WeatherUndergroundApi
         end
 
         def get_xml(url)
-            #todo
+            # todo: Get the xml stuff to work and not just json.
         end
 
         def to_params(hsh = {})
@@ -161,6 +295,10 @@ module WeatherUndergroundApi
             value = value.join(" ") if value.is_a?(Array)
             value = ERB::Util.h(value) if escape
             %(#{key}="#{value}")
+        end
+
+        def icon_set_url
+            "http://icons.wxug.com/i/c/#{@options[:icon_set]}/%s.gif"
         end
     end
 end
